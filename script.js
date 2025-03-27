845ab00190757d670d13cd37104aec8d04ee2850
Kaio Rodrigues <kaio.rodrigues@clevertech.biz>
2025-03-27 20:58:19 -0300

    adding scriptt to fetch

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-function enviar(){
-    document.getElementById("form").submit();
-    cleanForm("form");  
-}
-
 function cleanForm(id){
     return document.getElementById(id)?.reset();
+}
+
+async function sendMessage(){
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const name1 = formData.get('name1');
+    const name2 = formData.get('name2'); 
+    const name3 = formData.get('name3');
+    const name4 = formData.get('name4');
+    const name5 = formData.get('name5');
+    const hist = formData.get('hist');
+    const names = [name1, name2, name3, name4, name5]
+    const message = hist
+    const body = {
+        names: names,
+        message: message
+    }
+
+    alert(body)
+
+    fetch("https://fsdt-contact.onrender.com/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body,
+    })
+    .then(response => {
+        response.json()
+        cleanForm('form')
+    })
+    .then(result => alert("Desafio Concluido:", result))
+    .catch(error => console.error("Erro no Desafio:", error));
+
 }
